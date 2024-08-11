de8583988f20361c78ed97c68e319aa43fcf923a
0Bumblehub0 <hiteskumar2102000@gmail.com>
2024-08-11 19:20:47 +0530

    add new code

--- Basics.cpp
+++ Basics.cpp
@@ -4,3 +4,11 @@ int main(){
     cout << "Hello World";
     return 0;
 }
+
+#include <iostrem>
+using namespace std;
+int main(){
+    int string str= "Hello";
+    cout << str << endl;
+    return 0;
+}
